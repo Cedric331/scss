5ef6755414dbea878e3613e1c5320447ed1856c2
Cedric Lima <limacedric@hotmail.fr>
2021-03-03 16:36:22 +0100

    Création du dossier sass

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="style/sass/style.css" rel="stylesheet">
+    
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <header>
+    <h1>Hello, world!</h1>
+  </header>
+
+  <div id="logo">
+      LOGO
+  </div>
+
+  <div id="slogan">
+      SLOGAN
+  </div>
+
+  <div class="alert-green">Alert</div>
+  <div class="alert-red">Alert</div>
+
+  <footer>
+   <h1>Footer</h1>
+  </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  </body>
+</html>
